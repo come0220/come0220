06961f585be9bf8c441298872d59174d096dc3bf
Your Name <come0220@gmail.com>
2012-04-22 12:35:00 +0900

    modified:   memo/app/controllers/memolist_controller.rb 	modified:   memo/app/views/layouts/application.html.erb 	modified:   memo/app/views/memolist/confirm.html.erb modified:   memo/app/views/memolist/dblist.html.erb 	modified:   memo/app/views/memolist/error.html.erb

--- rails/memo/app/controllers/memolist_controller.rb
+++ rails/memo/app/controllers/memolist_controller.rb
@@ -13,43 +13,46 @@ class MemolistController < ApplicationController
 	def getdata
 	    require 'hpricot'
     	require 'open-uri' 
+
         #名前、ページ数取得
     	@name = params[:name][:name]
         @number = params[:number].to_i
+        if @name !~ /[0-9A-Za-z]+$/ then
+          render :action => 'error'  and return
+        end
+        unless @name.blank?
         #スペースをプラスに置換
         @name = @name.gsub(/(\s)/,"+")
-        unless @name.blank?
         #Hpricot
-          doc = Hpricot(open("http://www.xvideos.com/?k=#{@name}&p=#{@number}&sort=rating")) 
-          tmp = (doc/"a.miniature").map {|href| href[:href]}
-          @img = (doc/"img").map {|img| img[:src]}
-          @img.delete("http://img100.xvideos.com/videos/thumbs/xvideos.gif")
+        doc = Hpricot(open("http://www.xvideos.com/?k=#{@name}&p=#{@number}&sort=rating")) 
+        tmp = (doc/"a.miniature").map {|href| href[:href]}
+        @img = (doc/"img").map {|img| img[:src]}
+        @img.delete("http://img100.xvideos.com/videos/thumbs/xvideos.gif")
         #ページ数取得
-          page = (doc/"div#pag"/:a).map {|href| href[:href]}
-          @page = page.length/2
-          i=0
-          @result = Array.new();
+        page = (doc/"div#pag"/:a).map {|href| href[:href]}
+        @page = page.length/2
+        i=0
+        @result = Array.new();
         #url取得
           tmp.each do |tmp|
-          tmp2 = tmp[28,7]
-          if tmp2[tmp2.length-1] == "/" then
-            tmp2 = tmp[28,6]
-          end
-          @result[i] = "http://www.xvideos.com/sitevideos/flv_player_site_v4.swf?id_video=#{tmp2}"
-          i+=1
+            tmp2 = tmp[28,7]
+            if tmp2[tmp2.length-1] == "/" then
+              tmp2 = tmp[28,6]
+            end
+            @result[i] = "http://www.xvideos.com/sitevideos/flv_player_site_v4.swf?id_video=#{tmp2}"
+            i+=1
           end
           render :action => 'confirm'  
-        else
+          else
           render :action => 'error'
-        end
-
+          end
     end
 
     def rescue_action_in_public(exception)
         case exception
            when ActiveRecord::RecordNotFound, ::ActionController::UnknownAction, ::ActionController::RoutingError, ActionView::TemplateError, NoMethodError
           render :action => 'error'
-        end
+          end
     end
 
     def makedb
